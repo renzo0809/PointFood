fdf081eb08d760926661f0237b10f29802b439f6
SteamPoweredDee <37774736+SteamPoweredDee@users.noreply.github.com>
2020-05-06 16:39:47 -0500

    Repositories
    Basic repositories

---
+++ src/main/java/com/example/repository/TarjetaRepository.java
@@ -0,0 +1,9 @@
+package com.example.repository;
+
+import com.example.entity.Tarjeta;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TarjetaRepository extends JpaRepository<Tarjeta,Long> {
+}
